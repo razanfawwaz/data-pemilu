6e43ac5bb35c7829da5d78e219a18c3087aae71d
razanfawwaz <razanfawwaz1905@gmail.com>
2024-11-27 19:26:04 +0700

    fix: link

--- src/pages/gubernur.tsx
+++ src/pages/gubernur.tsx
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import { useState, useEffect } from "react";
 import provinces from "@/data/0.json";
 import { PieChart } from 'react-minimal-pie-chart';
+import Link from "next/link";
 
 interface Candidate {
   ts: string;
@@ -210,7 +211,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold mb-6">Hasil Pilkada 2024 - Pemilihan Gubernur/Wali Kota</h1>
         <p className="text-sm text-gray-600 dark:text-gray-400">The data that is displayed here is from the <a href="https://pilkada2024.kpu.go.id" target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-400">https://pilkada2024.kpu.go.id API</a>. For more accurate data, please visit the official website of <a href="https://pilkada2024.kpu.go.id" target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-400">KPU</a>.</p>
 
-        <a href="/" className="text-blue-600 dark:text-blue-400 py-2 px-4 mt-4 inline-block bg-blue-100 rounded-md">View Data Kepala Daerah</a>
+        <Link href="/kepala-daerah" className="text-blue-600 dark:text-blue-400 py-2 px-4 mt-4 inline-block bg-blue-100 rounded-md">View Data Kepala Daerah</Link>
     
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {/* Dropdowns */}
